72d3172c311ab1eaaf6349224032c130a8f32dfc
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2022-05-08 21:51:19 +0900

    shortcut

--- shell.nix
+++ shell.nix
@@ -11,6 +11,7 @@ pkgs.mkShell {
   shellHook = ''
     alias fmt='ormolu -i $(git ls-files "*.hs")'
     alias t="cabal run test"
+    alias b="cabal build"
     export PS1="\e[34;1m[dev]\W$ \e[0m"
   '';
 }
