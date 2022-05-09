23322fc91fe0d7f0bfdfe7431e12b5e5bdeb4338
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2022-05-10 07:51:11 +0900

    wakaran

--- test/Test.hs
+++ test/Test.hs
@@ -2,6 +2,7 @@
 
 module Main where
 
+import Text.Printf
 import Generator
 import Gera
 import Network.HTTP.Req
@@ -19,17 +20,20 @@ tests :: TestTree
 tests = testGroup "Tests" [properties, specs]
 
 properties :: TestTree
-properties = testGroup "Properties" [prop_buildQuery]
+properties = testGroup "Properties" [prop_dateToFormattedString]
 
-prop_buildQuery :: TestTree
-prop_buildQuery =
+prop_dateToFormattedString :: TestTree
+prop_dateToFormattedString =
   testGroup
-    "buildQuery"
-    [ SC.testProperty "builds from specified values" $
-        \ymd ->
-          let (y, m, d) = ymd :: ValidDateParams
-              date = Date (getValidYear y) (getValidMonth m) (getValidDay d)
-           in 1 == 1
+    "dateToFormattedString (DOESN'T WORK...)"
+    [ SC.testProperty "concates members with a hyphen and pads by zero" $
+        \y m d ->
+          let
+              yv = getValidYear (y :: ValidYear)
+              mv = getValidMonth (m :: ValidMonth)
+              dv = getValidDay (d :: ValidDay)
+              date = Date yv mv dv
+          in dateToFormattedString date == printf "%04d-%02d-%02d" yv mv dv
     ]
 
 specs :: TestTree
