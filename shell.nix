391ed5f047306c9c93887582a8d7a2cf8567781d
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2022-05-14 10:43:16 +0900

    fix tsc setting

--- shell.nix
+++ shell.nix
@@ -7,12 +7,18 @@ pkgs.mkShell {
     cabal-install
     ormolu
     zlib
+    nodejs
+    nodePackages.typescript
+    nodePackages.typescript-language-server
+    chromium
   ];
   shellHook = ''
     alias fmt='ormolu -i $(git ls-files "*.hs")'
     alias t="cabal run test --"
     alias b="cabal build"
     alias run="cabal run gera-index"
+    alias aggregate="node aggregator/dist/App.js"
     export PS1="\e[34;1m[dev]\W$ \e[0m"
+    export PUPPETEER_EXECUTABLE_PATH=`which chromium`
   '';
 }
