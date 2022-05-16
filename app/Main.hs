fd1ca8e86d5b9d1a931d896aac6095dabfdd722b
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2022-05-16 21:38:41 +0900

    utf8

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Twitter
+import Network.HTTP.Req
+import qualified Data.Text as Tx
+import qualified Data.Text.IO as Tx
 
 main :: IO ()
 main = do
   client <- fromDotEnv
   let sc = SearchCriteria 10 (Just (Datetime 2022 4 1 0 0 0)) Nothing
   resp <- fetchTwitter client sc
-  print resp
+  let tweets = responseBody resp :: Tweets
+  Tx.putStrLn $ Tx.intercalate "\n" [getText t | t <- getTweets tweets]
