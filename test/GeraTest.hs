104a624702d3071b451d07c2831fbb8fdb5d0191
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2022-05-23 07:35:29 +0900

    added test about audio tag finder

--- test/GeraTest.hs
+++ test/GeraTest.hs
@@ -2,6 +2,7 @@
 
 module GeraTest (tests) where
 
+import Text.HTML.TagSoup
 import qualified Data.ByteString.Lazy as L
 import Gera
 import System.IO.Unsafe
@@ -17,13 +18,21 @@ specs =
     map
       unsafePerformIO
       [ spec_parsePage
+      , spec_findAudioUrl
       ]
+
+spec_findAudioUrl :: IO TestTree
+spec_findAudioUrl =
+  HS.testSpec "findAudioUrl" $ do
+    it "can find url from an audio tag" $ do
+      tag <- L.readFile "test/gera-data/audiotag.html"
+      findAudioUrl (parseTags tag) `shouldBe` Right "https://firebasestorage.googleapis.com/v0/b/gera-prd.appspot.com/o/episode-audios%2Ff5Zq7fsnkbFSIbfvEAmw.mp3?alt=media"
 
 spec_parsePage :: IO TestTree
 spec_parsePage =
   HS.testSpec "parsePage" $ do
     it "can find audio source url" $ do
-      content <- L.readFile "test/gera/page.html"
+      content <- L.readFile "test/gera-data/page.html"
       parsePage content
         `shouldBe` Right
           ( Gera
