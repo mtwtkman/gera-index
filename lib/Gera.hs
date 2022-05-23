ac66ca7f68829f5f5cba5d2d42d7fb0016425f32
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2022-05-23 22:02:19 +0900

    episode parser

--- lib/Gera.hs
+++ lib/Gera.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE QuasiQuotes #-}
 module Gera where
 
 import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.Char8 as L
 import qualified Data.Text as Tx
 import Network.HTTP.Req
 import Text.HTML.TagSoup
+import Text.RE.TDFA.ByteString.Lazy
 
 data GeraError
   = FailedParse
@@ -26,11 +29,9 @@ fetchPage :: Url 'Https -> IO LbsResponse
 fetchPage url = runReq defaultHttpConfig $ do
   req GET url NoReqBody lbsResponse mempty
 
-data Episode = Episode
-  { getTitle :: Tx.Text,
-    getNumber :: Int
-  }
-  deriving (Show, Eq)
+data Episode = Episode { getTitle :: L.ByteString
+                       , getNumber :: Int
+                       } deriving (Show, Eq)
 
 data Gera = Gera
   { getEpisode :: Episode,
@@ -45,7 +46,14 @@ findAudioUrl tags = case dropWhile (~/= ("<audio>" :: String)) tags of
   x : _ -> Right $ fromAttrib "src" x
 
 findEpisode :: [Tag L.ByteString] -> ThrowsError Episode
-findEpisode tags = undefined
+findEpisode tags = case dropWhile (~/= ("<div class=episode-title>" :: String)) tags of
+                    [] -> Left NotFoundEpisode
+                    x : _ ->
+                      let s  = fromTagText x
+                          [[lbsNumber, title]] = s =~ [re|#([0-9]+) (.+)|] :: [[L.ByteString]]
+                     in case L.readInt lbsNumber of
+                      Just (number, _) -> Right (Episode title number)
+                      _ -> Left NotFoundEpisode
 
 parsePage :: L.ByteString -> ThrowsError Gera
 parsePage content =
